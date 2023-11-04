cef96a8a6cbbccae95edb308a8c853d4d8a9a43a
Fredrik Leonard Stenersen <fredrlst@stud.ntnu.no>
2023-11-04 12:04:54 +0100

    Deploy test workflow

--- OpenTerraAS/infra/variables.tf
+++ OpenTerraAS/infra/variables.tf
@@ -9,44 +9,6 @@ variable "project_name" {
   description = "Navn på prosjektet"
 }
 
-######## Backend variabler ########
-variable "backend_rg_name" {
-  type        = string
-  description = "Name of the backend resource group"
-}
-
-variable "backend_rg_location" {
-  type        = string
-  description = "Location of the backend resource group"
-}
-
-variable "backend_sa_basename" {
-  type        = string
-  description = "Name of the backend storage account"
-}
-
-variable "backend_container_name" {
-  type        = string
-  description = "Name of the backend storage container"
-}
-
-variable "backend_kv_basename" {
-  type        = string
-  description = "Name of the backend key vault"
-}
-
-variable "backend_kv_location" {
-  type        = string
-  description = "Location of the backend key vault"
-}
-
-variable "backend_sa_access_key_name" {
-  type        = string
-  description = "Name of the backend storage account access key"
-  sensitive   = true
-}
-
-
 ######## Lagring variabler ########
 variable "lagring_rgname" {
   type        = string
