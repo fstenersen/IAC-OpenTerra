718a303a12d229f281cdd185351be26d92d706ab
Fredrik Leonard Stenersen <fredrlst@stud.ntnu.no>
2023-11-05 23:44:54 +0100

    Changing local variable

--- OpenTerraAS/infra/keyvault/locals.tf
+++ OpenTerraAS/infra/keyvault/locals.tf
@@ -3,6 +3,7 @@ locals {
 
   keyvault_rgname = "${var.keyvault_rgname}-${local.workspaces_suffix}"
   keyvault_name = "${var.keyvault_name}${local.workspaces_suffix}"
+  keyvault_access_key_name = "${var.keyvault_access_key_name}${local.workspaces_suffix}"
 
   common_tags = {
     Workspace = terraform.workspace
